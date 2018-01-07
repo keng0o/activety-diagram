a60a4e8d44b9bbd058bc3fae832394ef3306d873
kengo nakamura <hh70024@gmail.com>
2018-01-07 16:23:42 +0900

    default content

--- src/config/content.js
+++ src/config/content.js
@@ -1,115 +1,142 @@
 export default `
----
+  graph [
+  rankdir = "LR"
+  ];
+  node [
+  fontsize = "16"
+  shape = "ellipse"
+  ];
+  edge [
+  ];
+  "node0" [
+  label = "<f0> 0x10ba8| <f1>"
+  shape = "record"
+  ];
+  "node1" [
+  label = "<f0> 0xf7fc4380| <f1> | <f2> |-1"
+  shape = "record"
+  ];
+  "node2" [
+  label = "<f0> 0xf7fc44b8| | |2"
+  shape = "record"
+  ];
+  "node3" [
+  label = "<f0> 3.43322790286038071e-06|44.79998779296875|0"
+  shape = "record"
+  ];
+  "node4" [
+  label = "<f0> 0xf7fc4380| <f1> | <f2> |2"
+  shape = "record"
+  ];
+  "node5" [
+  label = "<f0> (nil)| | |-1"
+  shape = "record"
+  ];
+  "node6" [
+  label = "<f0> 0xf7fc4380| <f1> | <f2> |1"
+  shape = "record"
+  ];
+  "node7" [
+  label = "<f0> 0xf7fc4380| <f1> | <f2> |2"
+  shape = "record"
+  ];
+  "node8" [
+  label = "<f0> (nil)| | |-1"
+  shape = "record"
+  ];
+  "node9" [
+  label = "<f0> (nil)| | |-1"
+  shape = "record"
+  ];
+  "node10" [
+  label = "<f0> (nil)| <f1> | <f2> |-1"
+  shape = "record"
+  ];
+  "node11" [
+  label = "<f0> (nil)| <f1> | <f2> |-1"
+  shape = "record"
+  ];
+  "node12" [
+  label = "<f0> 0xf7fc43e0| | |1"
+  shape = "record"
+  ];
+  "node0":f0 -> "node1":f0 [
+  id = 0
+  ];
+  "node0":f1 -> "node2":f0 [
+  id = 1
+  ];
+  "node1":f0 -> "node3":f0 [
+  id = 2
+  ];
+  "node1":f1 -> "node4":f0 [
+  id = 3
+  ];
+  "node1":f2 -> "node5":f0 [
+  id = 4
+  ];
+  "node4":f0 -> "node3":f0 [
+  id = 5
+  ];
+  "node4":f1 -> "node6":f0 [
+  id = 6
+  ];
+  "node4":f2 -> "node10":f0 [
+  id = 7
+  ];
+  "node6":f0 -> "node3":f0 [
+  id = 8
+  ];
+  "node6":f1 -> "node7":f0 [
+  id = 9
+  ];
+  "node6":f2 -> "node9":f0 [
+  id = 10
+  ];
+  "node7":f0 -> "node3":f0 [
+  id = 11
+  ];
+  "node7":f1 -> "node1":f0 [
+  id = 12
+  ];
+  "node7":f2 -> "node8":f0 [
+  id = 13
+  ];
+  "node10":f1 -> "node11":f0 [
+  id = 14
+  ];
+  "node10":f2 -> "node12":f0 [
+  id = 15
+  ];
+  "node11":f2 -> "node1":f0 [
+  id = 16
+  ];
+`;
+/*
+  export default `
+subgraph cluster_0 {
+  style=filled;
+  color=lightgrey;
+  node [style=filled,color=white];
+  a0 -> a1 -> a2 -> a3;
+  label = "process #1";
+}
 
-# h1 Heading 8-)
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-## Horizontal Rules
-___
----
-***
-## Typographic replacements
-Enable typographer option to see result.
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
-test.. test... test..... test?..... test!....
-!!!!!! ???? ,,  -- ---
-"Smartypants, double quotes" and 'single quotes'
-## Emphasis
-**This is bold text**
-__This is bold text__
-*This is italic text*
-_This is italic text_
-~~Strikethrough~~
-## Blockquotes
-> Blockquotes can also be nested...
->> ...by using additional greater-than signs right next to each other...
-> > > ...or with spaces between arrows.
-## Lists
-Unordered
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet
-    - Nulla volutpat aliquam velit
-+ Very easy!
-Ordered
-1. Lorem ipsum dolor sit amet
-2. Consectetur adipiscing elit
-3. Integer molestie lorem at massa
-1. You can use sequential numbers...
-1. ...or keep all the numbers as \`1.\`
-Start numbering with offset:
-57. foo
-1. bar
-## todo list
-- [ ] Lorem ipsum dolor sit amet
-- [x] Consectetur adipiscing elit
-## Code
-Inline \`code\`
-Indented code
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-Block code "fences"
-\`\`\`
-Sample text here...
-\`\`\`
-Syntax highlighting
-\`\`\`javascript
-var foo = function (bar) {
-  return bar++;
-};
-console.log(foo(5));
-\`\`\`
-\`\`\`python
-def fn():
-    print('abc')
-\`\`\`
-## Tables
-| Option | Description |
-| ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-Right aligned columns
-| Option | Description |
-| ------:| -----------:|
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-## Links
-[link text](https://github.com/eteplus/menote)
-[link with title](https://github.com/eteplus/menote "markdown-editor")
-## Images
-![Minion](https://octodex.github.com/images/minion.png)
-![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
-Like links, Images also have a footnote style syntax
-![Alt text][id]
-With a reference later in the document defining the URL location:
-[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
-### [Emojies](https://github.com/eteplus/menote)
-> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum:
->
-> Shortcuts (emoticons): :-) :-( 8-) ;)
-### [Subscript](https://github.com/eteplus/menote) / [Superscript](https://github.com/eteplus/menote)
-- 19^th^
-- H~2~O
-### [\\<ins>](https://github.com/eteplus/menote)
-++Inserted text++
-### [\\<mark>](https://github.com/eteplus/menote)
-==Marked text==
-### [Footnotes](https://github.com/eteplus/menote)
-Footnote 1 link[^first].
-Footnote 2 link[^second].
-Inline footnote^[Text of inline footnote] definition.
-Duplicated footnote reference[^second].
-[^first]: Footnote **can have markup**
-    and multiple paragraphs.
-[^second]: Footnote text.
+subgraph cluster_1 {
+  node [style=filled];
+  b0 -> b1 -> b2 -> b3;
+  label = "process #2";
+  color=blue
+}
+start -> a0;
+start -> b0;
+a1 -> b3;
+b2 -> a3;
+a3 -> a0;
+a3 -> end;
+b3 -> end;
 
-`
+start [shape=Mdiamond];
+end [shape=Msquare];
+`;
+*/

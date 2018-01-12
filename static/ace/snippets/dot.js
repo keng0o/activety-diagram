0523faaf3ed02d8a488695259147fe08d7a940c8
kengo nakamura <hh70024@gmail.com>
2018-01-12 21:02:30 +0900

    feat: fix dot snippets

--- static/ace/snippets/dot.js
+++ static/ace/snippets/dot.js
@@ -30,10 +30,11 @@ snippet @ret\n\
 	@return {${1:type}} ${2:description}\n\
 # Next\n\
 snippet --\n\
-  -> ${1?:next page}\n\
-# Next\n\
-snippet --\n\
 	-> ${1?:next page}\n\
+# Variable\n\
+snippet var\n\
+	${1:shortname} [label=${2:$1}, shape=${3:|square,rectangle,circle,ellipse,triangle,plaintext,point,diamond,pentagon,hexagon,septagon,octagon,egg,trapezium,parallelogram,house,doublecircle,doubleoctagon,tripleoctagon,invtriangle,invtrapezium,invhouse,Mdiamond,Msquare,Mcircle,none,note,tab,folder,box3d|}${4:, URL=${5:http://en.wikipedia.org/wiki/John de Fries}}]\n\
+	${0}\n\
 ";
   exports.scope = "dot";
 });

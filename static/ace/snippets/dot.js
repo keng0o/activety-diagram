a8578ec5d29b9bbada92dd56b6fcbb8722171bee
kengo nakamura <hh70024@gmail.com>
2018-01-12 21:00:17 +0900

    feat: add dot snippets

--- static/ace/snippets/dot.js
+++ static/ace/snippets/dot.js
@@ -1,1 +1,39 @@
-define("ace/snippets/dot",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="dot"})
+define("ace/snippets/dot", ["require", "exports", "module"], function(
+  require,
+  exports,
+  module
+) {
+  "use strict";
+
+  exports.snippetText =
+    "# Page\n\
+snippet page\n\
+	${1:ページ名} -> ${2:遷移先ページ}\n\
+# Function\n\
+snippet func\n\
+	function ${1?:function_name}(${2:argument}) {\n\
+		${3:// body...}\n\
+  }\n\
+# tertiary conditional\n\
+snippet ter\n\
+	${1:/* condition */} ? ${2:a} : ${3:b}\n\
+# docstring\n\
+snippet /**\n\
+	/**\n\
+	 * ${1:description}\n\
+	 *\n\
+	 */\n\
+snippet @par\n\
+regex /^\\s*\\*\\s*/@(para?m?)?/\n\
+	@param {${1:type}} ${2:name} ${3:description}\n\
+snippet @ret\n\
+	@return {${1:type}} ${2:description}\n\
+# Next\n\
+snippet --\n\
+  -> ${1?:next page}\n\
+# Next\n\
+snippet --\n\
+	-> ${1?:next page}\n\
+";
+  exports.scope = "dot";
+});
